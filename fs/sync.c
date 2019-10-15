2adcedf4a9ee66eccc526130703ae57cc0cb9768
Khusika Dhamar Gusti <mail@khusika.com>
2019-10-15 20:49:35 +0700

    fs: sync: set proper permission for fysnc_enabled
    0755 brings invalid permisson reported by dmesg,
    also update the code sytle.
    
    Change-Id: I210cddb37efe05ab24484e3323912efd6e1e6fdc
    Signed-off-by: Khusika Dhamar Gusti <mail@khusika.com>
    Signed-off-by: prorooter007 <shreyashwasnik112@gmail.com>

--- fs/sync.c
+++ fs/sync.c
@@ -23,7 +23,7 @@
 #endif
 
 bool fsync_enabled = true;
-module_param(fsync_enabled, bool, 0755);
+module_param(fsync_enabled, bool, 0644);
 
 #define VALID_FLAGS (SYNC_FILE_RANGE_WAIT_BEFORE|SYNC_FILE_RANGE_WRITE| \
 			SYNC_FILE_RANGE_WAIT_AFTER)
@@ -217,7 +217,7 @@ SYSCALL_DEFINE1(syncfs, int, fd)
  */
 int vfs_fsync_range(struct file *file, loff_t start, loff_t end, int datasync)
 {
-	struct inode *inode = file->f_mapping->host;
+	struct inode *inode;
 
 #ifdef CONFIG_DYNAMIC_FSYNC
 	if (dyn_fsync_active && suspend_active)
@@ -227,14 +227,17 @@ int vfs_fsync_range(struct file *file, loff_t start, loff_t end, int datasync)
 	if (!fsync_enabled)
 		return 0;
 
+	inode = file->f_mapping->host;
 	if (!file->f_op->fsync)
 		return -EINVAL;
+
 	if (!datasync && (inode->i_state & I_DIRTY_TIME)) {
 		spin_lock(&inode->i_lock);
 		inode->i_state &= ~I_DIRTY_TIME;
 		spin_unlock(&inode->i_lock);
 		mark_inode_dirty_sync(inode);
 	}
+
 	return file->f_op->fsync(file, start, end, datasync);
 }
 EXPORT_SYMBOL(vfs_fsync_range);
